6f40fc43acc7ac976ed7198556992d3d45dabefb
Bart Massey <bart@cs.pdx.edu>
2019-01-14 13:37:13 -0800

    fixed unicode character example

--- uni.c
+++ uni.c
@@ -1,11 +1,14 @@
 #include <assert.h>
+#include <locale.h>
 #include <stdio.h>
 #include <wchar.h>
 
 int main() {
     // 0x1f60a == '😊'
-    wint_t smiley = 0x1f60a;
-    int n = wprintf("%lc\n", smiley);
+    wint_t smiley = L'😊';
+    // https://stackoverflow.com/a/41800614
+    setlocale(LC_ALL, "");
+    int n = printf("%lu 0x%x %lc\n", sizeof smiley, smiley, smiley);
     assert(n >= 0);
     return 0;
 }
