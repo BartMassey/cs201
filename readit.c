7ccdb8faded5a65014442ed487c830f8879b8b27
Bart Massey <bart@cs.pdx.edu>
2019-02-17 11:35:20 -0800

    added error check to readit

--- readit.c
+++ readit.c
@@ -2,6 +2,10 @@
 
 int main(int argc, char **argv) {
     FILE *f = fopen(argv[1], "r");
+    if (!f) {
+        perror("open");
+        return 1;
+    }
     while (1) {
         int ch = fgetc(f);
         if (ch == EOF)
