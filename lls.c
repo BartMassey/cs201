7c44cc97432252d0e7d63054cf03a939d763b515
Bart Massey <bart@cs.pdx.edu>
2019-02-17 21:07:19 -0800

    added lls

---
+++ lls.c
@@ -0,0 +1,24 @@
+#define _DEFAULT_SOURCE
+#include <assert.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main() {
+    DIR *d = opendir(".");
+    assert(d);
+    while (1) {
+        errno = 0;
+        struct dirent *e = readdir(d);
+        if (!e)
+            break;
+        printf("%s", e->d_name);
+        if (e->d_type == DT_DIR)
+            printf(" [dir]");
+        printf("\n");
+    }
+    extern int errno;
+    assert(errno == 0);
+    closedir(d);
+    return 0;
+}
